9f2c2cd0822f2ef05bac07d24043a06705c74af3
Thomas Mol <s130286@ap.be>
2024-09-20 20:48:49 +0200

    swap out testlogin component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./HomePage";
-import LoginTest from "./authentication/LoginTest";
+import Login from "./authentication/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/auth" element={<LoginTest />} />
+        <Route path="/auth" element={<Login />} />
       </Routes>
     </Router>
   );
